d8ba7bfd01d87249e22bff14655fa5385c5c1828
Emre Guneyler <eguneys@hotmail.com>
2021-05-16 20:56:52 +0000

    one study chapter

--- src/study.ts
+++ src/study.ts
@@ -3,3 +3,7 @@ import xhr from './xhr';
 export function allChapters(studyId: string) {
   return xhr.text(`/study/${studyId}.pgn`);
 }
+
+export function oneChapter(studyId: string, chapterId: string) {
+  return xhr.text(`/study/${studyId}/${chapterId}.pgn`);
+}
